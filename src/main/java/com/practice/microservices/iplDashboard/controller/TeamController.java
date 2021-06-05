bf1dd8bc1ccc2ed19e565d97b0492f6a797553a8
Shubham Ayachit <ayachitshubham@hotmail.com>
2021-06-05 18:38:37 +0530

    React Basic changes

--- src/main/java/com/practice/microservices/iplDashboard/controller/TeamController.java
+++ src/main/java/com/practice/microservices/iplDashboard/controller/TeamController.java
@@ -4,12 +4,14 @@ import com.practice.microservices.iplDashboard.model.Team;
 import com.practice.microservices.iplDashboard.repository.MatchRepository;
 import com.practice.microservices.iplDashboard.repository.TeamRepository;
 
-import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
     @Autowired
