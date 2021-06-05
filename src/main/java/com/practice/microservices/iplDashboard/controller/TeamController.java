6254077ac58a9fc220ebfcccf24c4f922563627b
Shubham Ayachit <ayachitshubham@hotmail.com>
2021-06-06 00:05:27 +0530

    Add Match API in spring boot

--- src/main/java/com/practice/microservices/iplDashboard/controller/TeamController.java
+++ src/main/java/com/practice/microservices/iplDashboard/controller/TeamController.java
@@ -1,5 +1,9 @@
 package com.practice.microservices.iplDashboard.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import com.practice.microservices.iplDashboard.model.Match;
 import com.practice.microservices.iplDashboard.model.Team;
 import com.practice.microservices.iplDashboard.repository.MatchRepository;
 import com.practice.microservices.iplDashboard.repository.TeamRepository;
@@ -8,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,5 +31,14 @@ public class TeamController {
         team.setMatches(matchRepository.findLatestMatches(teamName, 4));
         return team;
     }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year) {
+        LocalDate startDate = LocalDate.of(year, 1 , 1);
+        LocalDate endDate = LocalDate.of(year+1, 1 , 1);
+        List<Match> findByTeam1OrTeam2AndDateContaining = this.matchRepository.findByTeamNameForGivenDate(teamName, startDate, endDate);
+        return findByTeam1OrTeam2AndDateContaining;
+    }
+
 
 }
