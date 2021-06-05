4e315d4a63eb4639dbfbc590b0f37fc19a53c923
Shubham Ayachit <ayachitshubham@hotmail.com>
2021-06-05 22:08:36 +0530

    React app changes for Team Page

--- src/frontend/src/components/MatchDetailCard.js
+++ src/frontend/src/components/MatchDetailCard.js
@@ -1,12 +1,21 @@
 import { React } from 'react';
+import { Link } from 'react-router-dom';
 
-export const MatchDetailCard = ({match}) => {
+export const MatchDetailCard = ({teamName, match}) => {
   if(!match) return null;
+  const otherTeam = teamName === match.team1 ? match.team2 : match.team1;
+  const otherTeamRoute = `/teams/${otherTeam}`;
   return (
     <div className="MatchDetailCard">
-      <h3>Latest Matched</h3>
-      <h4>Match Details</h4>
-      <h4>{match.team1} vs {match.team2}</h4>
+      <h3>Latest Matches</h3>
+      <h1>Vs.
+        <Link to={otherTeamRoute}>
+          {otherTeam}
+        </Link>
+      </h1>
+      <h1>{match.date}</h1>
+      <h3>at {match.venue}</h3>
+      <h3>{match.matchWinner} won by {match.resultMargin} {match.result}</h3>
     </div>
   );
 }
